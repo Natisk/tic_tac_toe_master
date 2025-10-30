cb7e1814e43562c80b6702ba7ae0bb9cacfa214f
Vitaly Matseiko <realnatisk@gmail.com>
2025-10-30 09:09:07 +0100

    add comment to main module

--- lib/tic_tac_toe_master.rb
+++ lib/tic_tac_toe_master.rb
@@ -5,6 +5,8 @@ require_relative 'tic_tac_toe_master/game'
 require_relative 'tic_tac_toe_master/board'
 require_relative 'tic_tac_toe_master/player'
 
+# Main namespace for TicTacToeMaster gem
+# provides an entry point to start the interactive tic_tac_toe game
 module TicTacToeMaster
   def self.start
     Game.new.start
