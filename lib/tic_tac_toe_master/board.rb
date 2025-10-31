1ada6161b050a009a81cee6b1963910ff8e1d38e
Vitaly Matseiko <realnatisk@gmail.com>
2025-10-31 23:27:14 +0100

    board logic

---
+++ lib/tic_tac_toe_master/board.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+module TicTacToeMaster
+  # Represents the game board for Tic Tac Toe
+  class Board
+    attr_reader :grid
+
+    FIELD_SIZE = (1..9)
+
+    WIN_CONBINATIONS = [
+      [0, 1, 3], [3, 4, 5], [6, 7, 8],
+      [0, 3, 6], [1, 4, 7], [2, 5, 8],
+      [0, 4, 8], [2, 4, 6]
+    ]
+
+    def initialize
+      @grid = FIELD_SIZE.to_a
+    end
+
+    def draw
+      puts '-----------'
+
+      grid.each_slice(3) do |row|
+        puts " #{row.join(' | ')} "
+        puts '-----------'
+      end
+    end
+
+    def place(position, symbol)
+      return false unless valid_position?(position)
+
+      grid[position - 1] = symbol
+      true
+    end
+
+    def valid_position?(position)
+      FIELD_SIZE.cover?(position) && grid[position - 1].is_a?(Integer)
+    end
+
+    def full?
+      grid.none?(Integer)
+    end
+
+    def winner?(symbol)
+      WIN_CONBINATIONS.any? do |combo|
+        combo.all? { |index| grid[index] == symbol }
+      end
+    end
+  end
+end
