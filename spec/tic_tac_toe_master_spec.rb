105b280f3e8a05c98fa240a116d54eaf2a9172fe
Vitaly Matseiko <realnatisk@gmail.com>
2025-11-03 12:59:08 +0100

    init tests first for entrance file

--- spec/tic_tac_toe_master_spec.rb
+++ spec/tic_tac_toe_master_spec.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
+
 RSpec.describe TicTacToeMaster do
-  it "has a version number" do
+  it 'has a version number' do
     expect(TicTacToeMaster::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'creates a new Game instance and calls start' do
+    game_instance = instance_double(TicTacToeMaster::Game)
+    expect(TicTacToeMaster::Game).to receive(:new).and_return(game_instance)
+    expect(game_instance).to receive(:start)
+
+    described_class.start
   end
 end
