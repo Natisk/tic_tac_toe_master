8733098e3b6d9a421872cb580abe7d8fab0581b4
Vitaly Matseiko <realnatisk@gmail.com>
2025-11-10 16:30:02 +0100

    rewrite tests according to posibility having multiple UIs

--- spec/tic_tac_toe_master_spec.rb
+++ spec/tic_tac_toe_master_spec.rb
@@ -7,10 +7,25 @@ RSpec.describe TicTacToeMaster do
     expect(TicTacToeMaster::VERSION).not_to be nil
   end
 
-  it 'creates a new Game instance and calls start' do
+  it 'creates a new Game instance and calls UI start' do
     game_instance = instance_double(TicTacToeMaster::Game)
-    expect(TicTacToeMaster::Game).to receive(:new).and_return(game_instance)
-    expect(game_instance).to receive(:start)
+    ui_instance = instance_double(TicTacToeMaster::UI::ConsoleUI)
+
+    allow(TicTacToeMaster).to receive(:gets).and_return("Alice\n", "Bob\n")
+
+    # waiting for game to be created
+    expect(TicTacToeMaster::Game)
+      .to receive(:new)
+      .and_return(game_instance)
+
+    # waiting for UI to be created with the game
+    expect(TicTacToeMaster::UI::ConsoleUI)
+      .to receive(:new)
+      .with(game_instance)
+      .and_return(ui_instance)
+
+    # waiting for UI to call start
+    expect(ui_instance).to receive(:start)
 
     described_class.start
   end
