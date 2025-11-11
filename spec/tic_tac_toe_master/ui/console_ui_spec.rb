4564aa3fe49e353ea32975366bb091ec562a796f
Vitaly Matseiko <realnatisk@gmail.com>
2025-11-11 21:39:15 +0100

    specs for console_ui

---
+++ spec/tic_tac_toe_master/ui/console_ui_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe TicTacToeMaster::UI::ConsoleUI do
+  let(:board) { instance_double(TicTacToeMaster::Board, draw: nil) }
+  let(:player1) { instance_double(TicTacToeMaster::Player, name: 'Alice', symbol: 'X') }
+  let(:game) { instance_double(TicTacToeMaster::Game, board: board, current_player: player1) }
+
+  subject(:ui) { described_class.new(game) }
+
+  before do
+    allow($stdout).to receive(:puts)
+    allow($stdout).to receive(:print)
+  end
+
+  it 'asks for a move and calls make_move' do
+    allow(ui).to receive(:ask_for_move).and_return(5)
+    allow(game).to receive(:make_move).and_return(:draw)
+
+    expect(game).to receive(:make_move).with(5)
+
+    ui.start
+  end
+end
